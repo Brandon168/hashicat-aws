cd86745ee1bfa044263c1e93ff71848a8934da07
Brandon Elliott <brelliott@gmail.com>
2024-10-16 22:31:03 +0000

    abc123

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,8 @@ module "s3-bucket" {
   source  = "app.terraform.io/brandon-training2/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "brandonelliottmy-s3-bucket"
   acl    = "private"
-  bucket_prefix = "brandonelliott"
 
   versioning = {
     enabled = true
